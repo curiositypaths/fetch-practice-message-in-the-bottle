840a1795a8b93d361f606047573703f4157245c1
curiositypaths <curiositypaths@users.noreply.github.com>
2018-05-18 14:25:24 -0400

    Add create and view message(s)

---
+++ app/controllers/api/v1/messages_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::MessagesController < ApplicationController
+    def index
+        message = Message.all.sample
+        render json: message
+    end
+
+    def create
+        message = Message.create(message_params)
+        render json: message
+    end
+
+    def show
+        message = Message.find(params[:id])
+        render json: message
+    end
+
+    private
+    def message_params
+        params.require(:message).permit(:real_name,:message)
+    end
+end
